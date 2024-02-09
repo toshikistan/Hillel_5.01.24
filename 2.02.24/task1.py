017de9b686ee68d6d24e64d072a1012114503c3a
toshikistan <toshikistan@gmail.com>
2024-02-09 07:10:21 +0200

    a bit change

--- 2.02.24/task1.py
+++ 2.02.24/task1.py
@@ -1,31 +1,7 @@
 '''
 1. Даний текстовий файл. Необхідно створити новий файл, який потрібно переписати з першого файлу всі слова, що складаються не менше ніж з семи літер.
 
-2. Даний текстовий файл. Підрахувати кількість слів у ньому.
 
-3. Створіть програму, яка перевіряє текст на неприпустимі слова.
-
-Якщо неприпустиме слово знайдено, його слід замінити на набір символів *.
-
-За підсумками роботи програми необхідно показати статистику дій.
-
-Наприклад, якщо й у нас є такий текст:
-
-To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep
-
-Неприпустиме слово: die.
-
-Результат:
-
-To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To ***: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To ***, to sleep.
-
-Статистика: 2 заміни слова die.
-
-Нотатка: 
-
-Текст для всіх завдань можна написати самостійно або взяти з Інтернету.
-
-Логіка має працювати з будь-яким текстом.
 '''
 import os
 
